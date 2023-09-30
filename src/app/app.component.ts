7ab8deb0e8780738c14e9992936094c9e9119f3a
Lennon Reid <lenonrade@163.com>
2023-10-01 01:37:47 +0800

    build(app): upgrade to ng@11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
-import {NgForage} from 'ngforage';
+import { NgForage } from 'ngforage';
 
 export interface TableListInterface {
   size: number | string;
@@ -24,84 +24,72 @@ const FAKEDATA = [
 })
 export class AppComponent {
   title = 'import';
-  // accpt file type
+  // Acceptable file types
   acceptFile = '.xls,.xlsx';
-  // 文件大小
+  // File size
   sizeM = 0;
   sizeKb = 0;
-  // the data of import
+  // Imported data
   importUserList: any;
-  // spent time when upload before getting the data
+  // Time spent when uploading before getting the data
   period = 0;
-  // the type of time accept strong: s.ms.hour,min,day It will be 0 if the time low to 1 second一
+  // The type of time accepted (e.g., s, ms, hour, min, day). It will be 0 if the time is less than 1 second.
   type = 'ms';
-  // the begin time when click upload
+  // The start time when clicking upload
   beginTime: any;
-  // the info of headers
+  // Header information
   importUserHeader: any;
   fileList: any;
   tableList: TableListInterface[] = [];
-  // config the counts which is needed to export
-  // listNeededArr = [20000, 50000, 100000, 200000];
+  // Configure the counts needed for export
   listNeededArr = [4000000];
   tableHeader = [
     {
-      title: '文件大小',
+      title: 'File Size',
       key: 'size'
     },
     {
-      title: '表头条数',
+      title: 'Header Count',
       key: 'count'
     },
     {
-      title: '总条数',
+      title: 'Total Count',
       key: 'total'
     },
     {
-      title: '读取时间',
+      title: 'Read Time',
       key: 'time'
     },
     {
-      title: '操作时间',
+      title: 'Operation Time',
       key: 'operateTime'
     },
     {
-      title: '操作类型',
+      title: 'Operation Type',
       key: 'operateType'
     }
   ];
 
   constructor(
-    private ngForage: NgForage
   ) {
-    // this.exportFile(FAKEDATA);
     this.fileList = [];
     this.importUserHeader = [];
-    this.initTableList();
-    // this.exportList();
-    this.listNeededArr.forEach((count: number) => {
-      this.createData(count);
-    });
-  }
-
-  async initTableList(): Promise<void> {
-
-    this.tableList = await this.getFromForage() ? await this.getFromForage() : [];
+    // this.listNeededArr.forEach((count: number) => {
+    //   this.createData(count);
+    // });
   }
 
 
   /**
-   * 间隔时间
-   * @param faultDate 初始时间
-   * @param type 时间格式
+   * Calculate time interval
+   * @param faultDate Initial time
+   * @param type Time format
    */
   timePeriod(faultDate: any, type?: string): any {
     const completeTime = new Date();
-    // let d1 = new Date(faultDate);
-    // let d2 = new Date(completeTime);
     const stime = new Date(faultDate).getTime();
     const etime = new Date(completeTime).getTime();
-    const usedTime = etime - stime;  // 两个时间戳相差的毫秒数
+    const usedTime = etime - stime;  // Difference in milliseconds between two timestamps
 
     const days = Math.floor(usedTime / (24 * 3600 * 1000));
     const hours = Math.floor(usedTime / (3600 * 1000));
@@ -130,28 +118,25 @@ export class AppComponent {
     let remain = 0;
     if (count > 16384) {
       remain = Math.ceil(count / 16384);
-      const remainCount = (count % 16384);
+      const remainCount = count % 16384;
       for (let j = 0; j < remain; j++) {
         fakeDataUsed[j] = [];
-        for (let i = 0; i < (j === remain - 1 ? remainCount < 16384 ? remainCount : 16384 : 16384); i++) {
-          fakeDataUsed[j].push(i + '条');
+        for (let i = 0; i < (j === remain - 1 ? (remainCount < 16384 ? remainCount : 16384) : 16384); i++) {
+          fakeDataUsed[j].push(i + 'item');
         }
-
       }
     } else {
       for (let i = 0; i < count; i++) {
-        innerData.push(i + '条');
+        innerData.push(i + 'item');
       }
       fakeDataUsed.push(innerData);
     }
     if (fakeDataUsed.length > 0) {
       this.exportFile(fakeDataUsed, remain);
     }
-
-
   }
 
-  // 文件大小c
+  // Calculate file size
   fileSize(file: File, fileSizeType: string): any {
     switch (fileSizeType) {
       case 'm':
@@ -161,28 +146,26 @@ export class AppComponent {
       case 'b':
         return file.size.toFixed(2);
     }
-
   }
 
   /**
-   * 文件变化
-   * @param target 变化结果
+   * File change event
+   * @param target Result of the change
    */
   fileChange(target: any): any {
-
     this.beginTime = new Date();
     if (target && target.files) {
       for (const file of target.files) {
-        const fileName = file.name; // 获取文件名
-        const reader: FileReader = new FileReader(); // FileReader 对象允许Web应用程序异步读取存储在用户计算机上的文件
-        // 当读取操作成功完成时调用FileReader.onload
+        const fileName = file.name; // Get the file name
+        const reader: FileReader = new FileReader();
+        // When the reading operation is successfully completed, FileReader.onload is called
         reader.onload = (e: any) => {
           const bstr: string = e.target.result;
-          const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+          const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
           const wsname: string = wb.SheetNames[0];
           const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-          this.importUserList = XLSX.utils.sheet_to_json(ws, {header: 1}); // 解析出文件数据，可以进行后面操作
-          this.importUserHeader = this.importUserList[0]; // 获得表头字段
+          this.importUserList = XLSX.utils.sheet_to_json(ws, { header: 1 });
+          this.importUserHeader = this.importUserList[0];
           this.fileList = this.fileList.concat(file);
 
           this.period = this.timePeriod(this.beginTime, this.type);
@@ -190,23 +173,19 @@ export class AppComponent {
           this.importUserList.forEach((list: any) => {
             total += list.length;
           });
-          console.log(total);
           this.sizeM = this.fileSize(file, 'm');
           this.sizeKb = this.fileSize(file, 'kb');
           this.tableList.unshift({
             size: this.sizeM + 'M',
             count: this.importUserHeader.length,
-            total: total + '条',
+            total: total + '\n' + 'items',
             time: this.period + this.type,
             operateTime: new Date().toLocaleDateString() + new Date().toLocaleTimeString(),
-            operateType: target.files.length > 1 ? '批量上传' : '单文件上传',
+            operateType: target.files.length > 1 ? 'Batch Upload' : 'Single File Upload',
           });
-          this.setToForage();
         };
         reader.readAsBinaryString(file);
-
       }
-
     }
   }
 
@@ -214,8 +193,6 @@ export class AppComponent {
     const dataSource = [];
     const headers = [];
     this.tableHeader.forEach((header: any) => {
-
-
       const arr2 = [];
       arr2.push(header.title);
       headers.push(arr2);
@@ -230,47 +207,29 @@ export class AppComponent {
       dataSource.push(listKeys);
     });
     this.exportFile(dataSource);
-
   }
 
   /**
-   * 导出
+   * Export data
    */
   exportFile(dataSource: any, remain?: number): any {
-    /* generate worksheet */
+    /* Generate worksheet */
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(dataSource);
-    // const ws2: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
 
-    /* generate workbook and add the worksheet */
+    /* Generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    // XLSX.utils.book_append_sheet(wb, ws2, 'Sheet2');
 
     let total = 0;
     dataSource.forEach((list: any) => {
       total += list.length;
     });
-    /* save to file */
-    XLSX.writeFile(wb, remain ? `${dataSource[0].length}列${remain}行_${total}条-${new Date().toLocaleDateString() + new Date().toLocaleTimeString()}.xlsx` : `${new Date().toLocaleDateString() + new Date().toLocaleTimeString()}测试结果.xlsx`, {type: 'file'});
-    // XLSX.readFile('SheetJS.xlsx',{type: 'file'});
-    // console.log(XLSX.readFile('SheetJS.xlsx'));
+    /* Save to file */
+    XLSX.writeFile(wb, remain ? `${dataSource[0].length} Columns ${remain} Rows ${total} Records - ${new Date().toLocaleDateString() + new Date().toLocaleTimeString()}.xlsx` : `${new Date().toLocaleDateString() + new Date().toLocaleTimeString()} Test Results.xlsx`, { type: 'file' });
   }
 
-
-  setToForage(): void {
-    this.ngForage.setItem('tableList', this.tableList).then(() => {
-
-      this.initTableList();
-    });
-  }
-
-  async getFromForage(): Promise<any> {
-    return await this.ngForage.getItem('tableList');
-  }
 
   clear(): void {
-    this.ngForage.clear();
-    this.initTableList();
+    this.tableList = [];
   }
 }
-
